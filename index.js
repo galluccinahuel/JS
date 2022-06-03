a01c23718b77f7dea2c7e8f46b02216b7ed9be6e
galluccinahuel <galluccinahuel@gmail.com>
2022-06-02 22:05:14 -0300

    asd

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let m_clave = "esc";
+let m_input =  prompt("Escríbe la palabra secreta.");
+
+
+while (m_input != m_clave)
+{
+    alert("Pista: ESCríbe otra palabra.");
+    m_input = prompt("Otra palabra");
+
+    if (m_input == m_clave) 
+    {
+        alert("Ganaste :D");
+    }
+    else
+    {
+        alert("Esa no es la palabra mágica");
+    }
+
+}
